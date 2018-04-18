dec9fb32c6eea60634e4e9f0acb8d40a9bdf4654
Chris Alberti <christopherjalberti@gmail.com>
2018-04-18 23:42:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 # Write your code here.
 
 def line(katz_deli)
-  katz_deli
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  end
 end
 
 def take_a_number(katz_deli, name)
@@ -9,5 +11,5 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  
+
 end
